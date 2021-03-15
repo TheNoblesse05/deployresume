b2fe2bb7284b131ed4a79c0e239a1a4ef354ca3e
Tilwani <vedant-pawan.tilwani@hpe.com>
2021-03-15 13:52:42 +0530

    changes commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { ImageComponentComponent } from './image-component/image-component.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ImageComponentComponent},
+  {path:'education', component:EducationComponent},
+  {path:'experience', component:ExperienceComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'hobbies', component:HobbiesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [EducationComponent,
+                                  ExperienceComponent,
+                                  ProjectsComponent,
+                                  HobbiesComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ImageComponentComponent } from './image-component/image-component.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    ImageComponentComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
